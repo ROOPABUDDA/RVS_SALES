4eb6d50cc9ee901bd3c2a8062c231740467ce8a7
ROOPA <roopabudda@gmail.com>
2018-07-17 11:58:32 +0530

    contacts with wtforms

--- app/main/contacts.py
+++ app/main/contacts.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
+from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField, StringField
 
 from wtforms import validators, ValidationError
 
 
 class contactSearchForm(FlaskForm):
-   emp_name = TextField("Name")
-   emp_organisation = TextField("Organisation")
-   emp_designation = TextField("Designation")
-   emp_location = TextField("Location")
-   emp_linkedin = TextField("Linkedin")
-   emp_decisionpower = TextField("Decision Power")
+   #emp_name = TextField("Name")
+   emp_name = StringField('emp_name', render_kw={"placeholder": "Name"})
+   emp_organisation = StringField('emp_organisation', render_kw={"placeholder": "Organisation"})
+   emp_designation = StringField('emp_designation', render_kw={"placeholder": "Designation"})
+   emp_location = StringField('emp_location', render_kw={"placeholder": "Location"})
+   emp_linkedin = StringField('emp_linkedin', render_kw={"placeholder": "Linkedin"})
+   emp_decisionpower = StringField('emp_decisionpower', render_kw={"placeholder": "Decision Power"})
 
    submit_search = SubmitField("Search")
 
